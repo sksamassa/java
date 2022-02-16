b9f315e9d3ddeecb153f518b00675c153dc92d99
Sekou Samassa <sksamassa@gmail.com>
2022-02-16 16:16:56 +0500

    initiation a github, version 1.2

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args)
         {
+            /**
+             * Initiation a github
+             */
             System.out.println("Hello world !");
         }
 }
